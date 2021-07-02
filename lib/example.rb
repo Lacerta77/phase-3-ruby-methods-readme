a6355d24e7c1f0bb329d6e5318859000a32324c6
lacerta <kevinhickman4@gmail.com>
2021-07-02 02:19:27 -0500

    Finished code along

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,40 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+  end
+  
+  say_hello_world_ten_times
+  
+  # ... The rest of the program
+  
+  say_hello_world_ten_times 
+
+
+  def greeting
+    # Leave a line break for the method body
+  end # Immediately close the method. 
+
+  def greeting
+    puts "Hello World" # Now code the body of the method.
+  end 
+
+  def greeting
+    puts "Hello World"
+  end
+  
+  greeting # Executing the method by name
+  #=> "Hello World"
+  
+  greeting # Executing the method again
+  #=> "Hello World" 
